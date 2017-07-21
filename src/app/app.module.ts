232301c74b7e5549a3ed830b281d97547c05fd15
brandon <bcooper085@gmail.com>
2017-07-21 12:50:28 -0700

    bootstrap golf app with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
